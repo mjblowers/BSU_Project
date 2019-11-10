4199f70fe50ff836af3932d32cda232cb742f70b
Cavan Cook <cavancook@u.boisestate.edu>
2019-11-09 20:26:10 -0700

    Cleaned up Styling

--- BSU_BackpackToGithub/BSU_BackpackToGithub/Startup.cs
+++ BSU_BackpackToGithub/BSU_BackpackToGithub/Startup.cs
@@ -82,7 +82,7 @@ namespace BSU_BackpackToGithub
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Login}/{id?}");
             });
         }
     }
